60403a3dd52200957a9c12c2fb0ebe8081a6eb37
何逸凡 <92091107+wjttdbx@users.noreply.github.com>
2023-07-20 21:39:22 +0800

    still test

--- measure_object_distance_Box_v8.py
+++ measure_object_distance_Box_v8.py
@@ -10,6 +10,7 @@ rs = RealsenseCamera()
 def parse_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="YOLOv8 live")
     parser.add_argument("--webcam-resolution", default=[1280, 720], nargs=2, type=int)
+    parser.add_argument("--label", default="", type=str)
     args = parser.parse_args()
     return args
 
